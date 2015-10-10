58096a24b5b1ca94c6ab012e039a7cf937b0b97a
Arkrothe <kapiljain.128@gmail.com>
2015-10-10 20:22:30 +0530

    Adding basic scritps for making tilemap

---
+++ Gravity Of The Situation/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+
+public class SceneManager : MonoBehaviour {
+    public TileMap TileMap;
+    public Transform WallPrefab;
+    public int MapLength = 32;
+    public int MapHeight = 32;
+	// Use this for initialization
+	void Start () {
+        TileMap = new TileMap(MapLength, MapHeight);
+        for (int i = 0; i < MapLength; i++)
+        {
+            for (int j = 0; j < MapHeight; j++)
+            {
+                
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Gravity Of The Situation/Assets/Scripts/Tilemap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TileMap
+    {
+        public List<GameObject>[,] Tiles;
+
+        public TileMap(int length, int height)
+        {
+            Tiles = new List<GameObject>[length, height];
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Tiles[i, j] = null;
+                }
+            }
+        }
+    }
+}
