6ae04ebf7f05e1a8c02634ba7c55435a5ce7a2bd
vefve <kubermehrotra@gmail.com>
2015-10-11 14:58:49 +0530

    Initializing maps.

--- Gravity Of The Situation/Assets/Scripts/SceneManager.cs
+++ Gravity Of The Situation/Assets/Scripts/SceneManager.cs
@@ -1,20 +1,23 @@
 ﻿using UnityEngine;
-using System.Collections;
-using Assets.Scripts;
+using System.Collections.Generic;
+//using Assets.Scripts;
 
 public class SceneManager : MonoBehaviour {
-    public TileMap TileMap;
+    public List<Transform>[,] sceneMap;
     public Transform WallPrefab;
-    public int MapLength = 32;
-    public int MapHeight = 32;
+    public int MapLength = 16;
+    public int MapHeight = 16;
+    private float scalingFactor = 0.8f;
 	// Use this for initialization
 	void Start () {
-        TileMap = new TileMap(MapLength, MapHeight);
-        for (int i = 0; i < MapLength; i++)
+        sceneMap = new SceneMaps(MapLength, MapHeight).getScene1();
+        for (int x = 0; x < MapLength; x++)
         {
-            for (int j = 0; j < MapHeight; j++)
+            for (int y = 0; y < MapHeight; y++)
             {
-                
+                if (sceneMap[x, y] != null) {
+                    Instantiate(sceneMap[x,y][1], new Vector3(x * scalingFactor, y * scalingFactor, 0), Quaternion.identity);
+                }
             }
         }
 	}
---
+++ Gravity Of The Situation/Assets/Scripts/SceneMaps.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SceneMaps {
+	private Transform WallPrefab;
+	private List<Transform>[,] CurrentScene;
+	private int length;
+	private int height;
+	public SceneMaps(int length, int height) {
+		this.length = length;
+		this.height = height;
+		CurrentScene = new List<Transform>[length, height];
+		for (int i = 0; i < length; i++)
+		{
+			for (int j = 0; j < height; j++)
+			{
+				CurrentScene[i, j] = null;
+			}
+		}
+	}
+		
+	public List<Transform>[,] getScene1 () {
+		List<Transform>[,] CurrentScene = new List<Transform>[length, height];
+		for (var x = 0; x < 16; x++) {
+			for (var y = 0; y < 16; y++) {
+				CurrentScene[x,y] = new List<Transform>(new Transform[] { WallPrefab });
+			}
+		}
+		return CurrentScene;
+	}
+}
